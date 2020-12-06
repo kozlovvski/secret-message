47cdd865d4c6def80240b79ac6406237adcbb6a2
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-12-06 15:43:34 +0100

    test(sign-in-button): extend SignInButton tests

--- src/features/auth/components/SignInButton/SignInButton.test.tsx
+++ src/features/auth/components/SignInButton/SignInButton.test.tsx
@@ -1,5 +1,8 @@
 import { shallow } from "enzyme";
+import { showAuthScreen } from "features/auth/auth.slice";
 import React from "react";
+import { mockDispatch, useSelector } from "react-redux";
+import defaultState from "store/default-state";
 
 import { findByTestAttr } from "test/testUtils";
 import SignInButton, { ISignInButtonProps } from "./SignInButton";
@@ -9,15 +12,25 @@ const defaultProps: ISignInButtonProps = {};
 describe("<SignInButton />", () => {
   const setup = (props?: Record<string, unknown>) =>
     shallow(<SignInButton {...defaultProps} {...props} />);
+  let wrapper: ReturnType<typeof setup>;
+
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockReturnValue(defaultState.auth);
+    wrapper = setup();
+  });
 
   test("should mount", () => {
-    const wrapper = setup();
     expect(wrapper.length).toBe(1);
   });
 
   test("should render without an error", () => {
-    const wrapper = setup();
     const component = findByTestAttr(wrapper, "component-SignInButton");
     expect(component.length).toBe(1);
+  });
+
+  test("should dispatch a correct action on click", () => {
+    const component = findByTestAttr(wrapper, "component-SignInButton");
+    component.simulate("click");
+    expect(mockDispatch).toBeCalledWith(showAuthScreen());
   });
 });
