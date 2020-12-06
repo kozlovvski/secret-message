55bad9488259994b2bf7eca6dc9d6b0a0b4ef761
mcbiggos@gmail.com <michal@kozlovv.ski>
2020-12-06 15:42:45 +0100

    refactor(templates): refactor component template test

--- templates/component/Component.test.tsx
+++ templates/component/Component.test.tsx
@@ -1,7 +1,9 @@
 import { shallow } from "enzyme";
 import React from "react";
+import { useSelector } from "react-redux";
+import defaultState from "store/default-state";
 
-import { findByTestAttr } from "../../test/testUtils";
+import { findByTestAttr } from "test/testUtils";
 import TemplateName, { ITemplateNameProps } from "./TemplateName";
 
 const defaultProps: ITemplateNameProps = {};
@@ -9,14 +11,18 @@ const defaultProps: ITemplateNameProps = {};
 describe("<TemplateName />", () => {
   const setup = (props?: Record<string, unknown>) =>
     shallow(<TemplateName {...defaultProps} {...props} />);
+  let wrapper: ReturnType<typeof setup>;
+
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockReturnValue(defaultState.auth);
+    wrapper = setup();
+  });
 
   test("should mount", () => {
-    const wrapper = setup();
     expect(wrapper.length).toBe(1);
   });
 
   test("should render without an error", () => {
-    const wrapper = setup();
     const component = findByTestAttr(wrapper, "component-TemplateName");
     expect(component.length).toBe(1);
   });
